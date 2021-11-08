f106aad644540c910046dc6b3bbcff3b38c9da75
Kyle Neil <kyle@johnneil.net>
2021-11-07 23:02:24 -0800

    added more tests to tester script

--- tester.sh
+++ tester.sh
@@ -4,7 +4,7 @@ check() {
 	inp_str=$1
 	expect_str=$2
 
-	out=$(echo $inp_str | ./smdsh -quiet | tr -d '\0') # ignore null characters as we don't see em
+	out=$(echo -e $inp_str | ./smdsh -quiet | tr -d '\0') # ignore null characters as we don't see em
 
 	if [[ "$out" == "$expect_str" ]]; then
 		echo PASS
@@ -13,10 +13,15 @@ check() {
 	fi
 }
 
+# check basic shell commands
 check "file README.md" "README.md: ASCII text"
+check "cd ..\ncd smdsh\nfile README.md\n" "README.md: ASCII text"
 
+# check variable storage
 check "get 0" "storing \"\""
-check "exit" ""
+check "set 0 README.md\nfile \$0\n" "README.md: ASCII text"
 
+# check builtins
+check "exit" ""
 check "" ""
 
